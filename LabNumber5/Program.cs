5d819c908028f4b868890df2ca73d1d38d9c288a
Aleena Tucker <lena.tucker@outlook.com>
2016-10-27 13:46:37 -0400

    Saving before trying different blocks as methods

--- LabNumber5/Program.cs
+++ LabNumber5/Program.cs
@@ -10,6 +10,27 @@ namespace LabNumber5
     {
         static void Main(string[] args)
         {
+            string YesNo;
+            int diceSides;
+
+            Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice? (y/n) ");
+            YesNo = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("How many sides does the dice have? ");
+            diceSides = int.Parse(Console.ReadLine());
+            Console.WriteLine(diceSides + " sided die-");
+            Console.WriteLine();
+
+            Random diceRoll1 = new Random();
+            int dice1 = diceRoll1.Next(1, diceSides);
+            Console.WriteLine("Dice 1: " + dice1);
+
+            Random diceRoll2 = new Random();
+            int dice2 = diceRoll2.Next(1, diceSides);
+            Console.WriteLine("Dice 2:" + dice2);
+
+
         }
     }
 }
